fcf9b2aa72f2770cedc55a05a7cb5b790ababdc3
ram-221 <rameshmalli857.com>
2022-10-26 22:25:37 +0530

    Player Start position

---
+++ src/com/bridgelabz/StratPosition.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class StratPosition {
+
+	public static void main(String[] args) {
+
+		int startPosition = 0; 
+	    System.out.println("Game start and Player is at Position " +startPosition);
+	}
+
+}
